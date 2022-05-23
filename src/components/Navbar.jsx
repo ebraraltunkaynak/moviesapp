7c530c1a39d0391ed6b5cdd98345ebd00d699586
Ebrar <16155052@mersin.edu.tr>
2022-05-23 14:59:39 +0300

    main cmp finish

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' flex items-center justify-between p-4 z-[100] w-full absolute'>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Ahmetflix</h1>
+        <div>
+            <button className='  px-6 py-2 rounded cursor-pointer text-white'>Sign In</button>
+            <button className='bg-red-600  px-6 py-2 round  red cursor-pointer text-white '>Sign Out</button>
+        </div>
+
+    </div>
+
+  )
+}
+
+export default Navbar
