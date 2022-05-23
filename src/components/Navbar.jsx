66bddba68f2035044ef0e0ba21b43fecb81d3fcc
Ebrar <16155052@mersin.edu.tr>
2022-05-23 17:51:00 +0300

    row finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className=' flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Ahmetflix</h1>
+        <h1 className='text-purple-600 text-4xl font-bold cursor-pointer'>MeaFlix</h1>
         <div>
             <button className='  px-6 py-2 rounded cursor-pointer text-white'>Sign In</button>
-            <button className='bg-red-600  px-6 py-2 round  red cursor-pointer text-white '>Sign Out</button>
+            <button className='bg-purple-600  px-6 py-2 round  red cursor-pointer text-white '>Sign Out</button>
         </div>
 
     </div>
